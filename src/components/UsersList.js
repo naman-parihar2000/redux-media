1b6093a8a0a073d60bf3509dc7af350e39b70947
Naman Singh Parihar <87593367+naman-parihar2000@users.noreply.github.com>
2023-06-25 14:43:53 +0530

    to fetch some data used thunk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUsers } from '../store'
 
 function UsersList() {
+  const dispatch = useDispatch();
+
+  useSelector((state) => {
+    return state.users;
+  })
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [dispatch])
   return (
     <div>UsersList!</div>
   )
+
 }
 
 export default UsersList
