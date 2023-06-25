ee3b6efa19001d8df71cfbfceaee67ee5606206a
Naman Singh Parihar <87593367+naman-parihar2000@users.noreply.github.com>
2023-06-25 07:17:36 +0530

    addpanel, button and users list components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import UsersList from './components/UsersList'
 
 function App() {
   return (
-    <div>App</div>
+    <h1 className="text-3xl font-bold underline">
+      <UsersList></UsersList>
+    </h1>
   )
 }
 
