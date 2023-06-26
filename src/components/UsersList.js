4047e1d4d7789abf598c247ce06a42d738af38a1
Naman Singh Parihar <87593367+naman-parihar2000@users.noreply.github.com>
2023-06-26 09:57:27 +0530

    created new users and rendered new users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,20 +1,52 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchUsers } from '../store'
+import { fetchUsers, addUser } from '../store'
+import Skeleton from './Skeleton'
 
 function UsersList() {
   const dispatch = useDispatch();
 
-  useSelector((state) => {
+  const { isLoading, data, error } = useSelector((state) => {
     return state.users;
   })
 
   useEffect(() => {
     dispatch(fetchUsers())
-  }, [dispatch])
+  }, [dispatch]);
+
+  const handleUserAdd = () => {
+    dispatch(addUser())
+  }
+
+  if (isLoading) {
+    return <Skeleton times={1} className="h-10 w-100"></Skeleton>
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className='mb-2 border rounded'>
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+          {user.name}
+        </div>
+      </div>
+    )
+  })
+
   return (
-    <div>UsersList!</div>
+    <div>
+      <div className="flex flx-row justify-between m-3">
+        <h1 className='m-2 text-xl'>USERS</h1>
+        <button onClick={handleUserAdd}>
+          + Add User
+        </button>
+      </div>
+      <div>{renderedUsers}</div>
+    </div>
   )
 
 }
--- src/store/index.js
+++ src/store/index.js
@@ -8,3 +8,4 @@ export const store = configureStore({
 })
 
 export * from './thunk/fetchUsers'
+export * from './thunk/addUser'
